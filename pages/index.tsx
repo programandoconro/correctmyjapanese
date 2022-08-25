e49feb2d8b129376d72b443f8457f585796fa46a
Ro <50117686+programandoconro@users.noreply.github.com>
2022-08-25 14:46:54 +0900

    Fixed English

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import CorrectMyWritting from "../components/correctMyWritting";
+import CorrectMyWriting from "../components/correctMyWriting";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Correct my writting</title>
+        <title>Correct my writing</title>
         <meta
           name="description"
-          content="A place to improve your writting skills in any language"
+          content="A place to improve your writing skills in any language"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -18,9 +18,9 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to{" "}
-          <b className="animate-pulse duration-75">correct my writting!</b>
+          <b className="animate-pulse duration-75">correct my writing!</b>
         </h1>
-        <CorrectMyWritting />
+        <CorrectMyWriting />
       </main>
 
       <footer className={styles.footer}></footer>
