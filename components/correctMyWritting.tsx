8015b0e7095363871006c470d22a66d4a231089d
Ro <50117686+programandoconro@users.noreply.github.com>
2022-08-25 14:15:09 +0900

    Correct my writing component

---
+++ components/correctMyWritting.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import InputArea from "./inputArea";
+import CONSTANTS from "../utils/constants";
+import Corrections from "./corrections";
+import findDifferences from "./findDifferences";
+
+const CorrectMyWritting = () => {
+  const [studentInput, setStudentInput] = useState<string>("");
+  const [teacherInput, setTeacherInput] = useState<string>("");
+  useEffect(() => {
+    const getPersistedData = async (route: "manuscripts" | "corrections") => {
+      const response = await fetch(`/api/${route}`, { method: "GET" });
+      const data = await response.json();
+      if (route === CONSTANTS.MANUSCRIPTS) {
+        setStudentInput(data.manuscripts);
+      } else if (route === CONSTANTS.CORRECTIONS) {
+        setTeacherInput(data.corrections);
+      }
+    };
+    getPersistedData(CONSTANTS.MANUSCRIPTS);
+    getPersistedData(CONSTANTS.CORRECTIONS);
+  }, []);
+  return (
+    <div className="grid gap-10 pt-10 w-screen xl:w-[1200px] px-10">
+      <InputArea
+        title={CONSTANTS.MANUSCRIPTS}
+        input={studentInput}
+        setInput={setStudentInput}
+      />
+      <InputArea
+        title={CONSTANTS.CORRECTIONS}
+        input={teacherInput}
+        setInput={setTeacherInput}
+      />
+      <Corrections
+        changes={findDifferences({
+          learnerSentence: studentInput,
+          teacherCorrection: teacherInput,
+        })}
+      />
+    </div>
+  );
+};
+export default CorrectMyWritting;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import Corrections from "../components/corrections";
-import findDifferences from "../components/findDifferences";
-import InputArea from "../components/inputArea";
 import styles from "../styles/Home.module.css";
-import CONSTANTS from "../utils/constants";
+import CorrectMyWritting from "../components/correctMyWritting";
 
 const Home: NextPage = () => {
-  const [studentInput, setStudentInput] = useState<string>("");
-  const [teacherInput, setTeacherInput] = useState<string>("");
-  useEffect(() => {
-    const getPersistedData = async (route: "manuscripts" | "corrections") => {
-      const response = await fetch(`/api/${route}`, { method: "GET" });
-      const data = await response.json();
-      if (route === CONSTANTS.MANUSCRIPTS) {
-        setStudentInput(data.manuscripts);
-      } else if (route === CONSTANTS.CORRECTIONS) {
-        setTeacherInput(data.corrections);
-      }
-    };
-    getPersistedData(CONSTANTS.MANUSCRIPTS);
-    getPersistedData(CONSTANTS.CORRECTIONS);
-  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -39,24 +20,7 @@ const Home: NextPage = () => {
           Welcome to{" "}
           <b className="animate-pulse duration-75">correct my writting!</b>
         </h1>
-        <div className="grid gap-10 pt-10 w-screen xl:w-[1200px] px-10">
-          <InputArea
-            title={CONSTANTS.MANUSCRIPTS}
-            input={studentInput}
-            setInput={setStudentInput}
-          />
-          <InputArea
-            title={CONSTANTS.CORRECTIONS}
-            input={teacherInput}
-            setInput={setTeacherInput}
-          />
-          <Corrections
-            changes={findDifferences({
-              learnerSentence: studentInput,
-              teacherCorrection: teacherInput,
-            })}
-          />
-        </div>
+        <CorrectMyWritting />
       </main>
 
       <footer className={styles.footer}></footer>
