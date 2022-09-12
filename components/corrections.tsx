b99bcf5c3ba4eeb47e7e2e2d1ff4023b212c254c
Ro <50117686+programandoconro@users.noreply.github.com>
2022-09-12 10:23:27 +0900

    Fixed display in corrections

--- components/corrections.tsx
+++ components/corrections.tsx
@@ -5,34 +5,30 @@ const Corrections = (props: { changes: Change[] }) => {
   return (
     <code className="grid gap-4 divide-y text-2xl bg-gray-800 mb-10 overflow-auto">
       <h2 className="text-4xl px-4 pt-2">Corrections:</h2>
-      <div className="flex p-4">
+      <div className="flex  flex-wrap p-4">
         {changes.map((change: Change, key) => {
           if (!change.added && !change.removed) {
-            return <p key={key}>{change.value}</p>;
+            return (
+              <p className="" key={key}>
+                {change.value}
+              </p>
+            );
           } else if (change.removed) {
             return (
-              <del
-                className="flex flex-wrap"
-                key={key}
-                style={{ color: "red" }}
-              >
+              <del className="" key={key} style={{ color: "red" }}>
                 {change.value}
               </del>
             );
           }
         })}
       </div>
-      <div className="flex p-4">
+      <div className="flex flex-wrap p-4">
         {changes.map((change: Change, key) => {
           if (!change.added && !change.removed) {
             return <p key={key}>{change.value}</p>;
           } else if (change.added) {
             return (
-              <p
-                className="flex flex-wrap"
-                key={key}
-                style={{ color: "green" }}
-              >
+              <p className="" key={key} style={{ color: "green" }}>
                 {change.value}
               </p>
             );
