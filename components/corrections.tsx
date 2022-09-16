de9322183d8e14349360008ffc32ff063618bf09
Ro <50117686+programandoconro@users.noreply.github.com>
2022-09-16 11:12:59 +0900

    media query for corrections font size

--- components/corrections.tsx
+++ components/corrections.tsx
@@ -3,7 +3,7 @@ import { Change } from "diff";
 const Corrections = (props: { changes: Change[] }) => {
   const { changes } = props;
   return (
-    <code className="gap-4 p-4 text-2xl bg-gray-800 mb-10 overflow-auto">
+    <code className="gap-4 p-4 text-sm sm:text-2xl  bg-gray-800 mb-10 overflow-auto">
       <h2 className="text-4xl px-4 pt-2">Corrections:</h2>
       <div className="p-4">
         {changes.map((change: Change, key) => {
