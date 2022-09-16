d3a51ac2855885bcd6f01026b22c1ac803996586
Ro <50117686+programandoconro@users.noreply.github.com>
2022-09-16 11:07:09 +0900

    improved layout of correction content

--- components/corrections.tsx
+++ components/corrections.tsx
@@ -3,9 +3,9 @@ import { Change } from "diff";
 const Corrections = (props: { changes: Change[] }) => {
   const { changes } = props;
   return (
-    <code className="gap-4 text-2xl bg-gray-800 mb-10 overflow-auto">
+    <code className="gap-4 p-4 text-2xl bg-gray-800 mb-10 overflow-auto">
       <h2 className="text-4xl px-4 pt-2">Corrections:</h2>
-      <span className="p-4">
+      <div className="p-4">
         {changes.map((change: Change, key) => {
           if (!change.added && !change.removed) {
             return (
@@ -21,7 +21,7 @@ const Corrections = (props: { changes: Change[] }) => {
             );
           }
         })}
-      </span>
+      </div>
       <hr className="my-4" />
 
       <div className="p-4">
