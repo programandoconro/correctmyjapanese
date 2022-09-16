c210a5848548c2e04da74c5c39ea5304e3a9e989
Ro <50117686+programandoconro@users.noreply.github.com>
2022-09-16 11:03:58 +0900

    improved correction layout

--- components/corrections.tsx
+++ components/corrections.tsx
@@ -3,26 +3,28 @@ import { Change } from "diff";
 const Corrections = (props: { changes: Change[] }) => {
   const { changes } = props;
   return (
-    <code className="gap-4 divide-y text-2xl bg-gray-800 mb-10 overflow-auto">
+    <code className="gap-4 text-2xl bg-gray-800 mb-10 overflow-auto">
       <h2 className="text-4xl px-4 pt-2">Corrections:</h2>
-      <span className="flex-wrap flex p-4">
+      <span className="p-4">
         {changes.map((change: Change, key) => {
           if (!change.added && !change.removed) {
             return (
-              <span className="grid" key={key}>
+              <span className="" key={key}>
                 {change.value}
               </span>
             );
           } else if (change.removed) {
             return (
-              <del className="grid" key={key} style={{ color: "red" }}>
+              <del className="" key={key} style={{ color: "red" }}>
                 {change.value}
               </del>
             );
           }
         })}
       </span>
-      <div className="flex flex-wrap p-4">
+      <hr className="my-4" />
+
+      <div className="p-4">
         {changes.map((change: Change, key) => {
           if (!change.added && !change.removed) {
             return (
