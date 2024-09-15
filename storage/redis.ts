acdce7f4b5d1deb1bca17724f3d109f5377a163d
programandoconro <programandoconro@gmail.com>
2024-09-15 13:21:42 +0900

    Update redis credentials

--- storage/redis.ts
+++ storage/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL || "", {
+const redis = new Redis(process.env.KV_URL || "", {
   tls: {
     rejectUnauthorized: false,
   },
